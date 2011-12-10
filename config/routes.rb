c892b5faa328aaa9178b869d2783c7c6a8ecb683
Nicholas Nuing <nicholas.nuing@gmail.com>
2011-12-10 16:21:10 +0800

    Changed the root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 PigeonMe::Application.routes.draw do
+  get "home/index"
+
+  get "home/new"
+
+  get "home/create"
+
+  get "home/edit"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +57,7 @@ PigeonMe::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
