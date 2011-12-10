4acc87815615a4880134727d0fa701c206613e46
Nicholas Nuing <nicholas.nuing@gmail.com>
2011-12-10 15:32:40 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PigeonMe::Application.config.secret_token = 'e233ed74e412ccd8a3dd279c34ebae47c293695774d4bc759b490b2241e38c017550a74b34ca9135fee5e95b55dd0b29ff5afd116b8062f0b45bd0f4c8ccbdc3'
